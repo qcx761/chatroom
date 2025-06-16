3e55fd0377054c7b6c079b6e1c323f9a312508d9
qcx761 <3216563979@qq.com>
2025-06-16 21:11:09 +0800

    1

--- chat-system/src/threadpoll/threadpoll.hpp
+++ chat-system/src/threadpoll/threadpoll.hpp
@@ -1,63 +1,52 @@
 #ifndef THREADPOOL_HPP
 #define THREADPOOL_HPP
-#include <bits/stdc++.h>
-using namespace std;
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <functional>
+#include <stdexcept>
+#include <type_traits>
+
 class threadpool {
-    public:
-    threadpool(size_t num):stop(false){
-        for(int i=0;i<num;i++){
-            this->workers.emplace_back([this]{
-                while(1){
-                    function<void()> task;
-                    {
-                    unique_lock<mutex> lock(this->mtx);
-                    this->condition.wait(lock,[this]{return !this->tasks.empty()||this->stop;});
-                    if(stop&&this->tasks.empty()) return;
-                    task=this->tasks.front();
-                    this->tasks.pop();
-                    }
-                    task();
-                }
-            });
-        }
-    }
-    ~threadpool(){
-        {
-            unique_lock<mutex> lock(this->mtx);
-            this->stop=true;
-        }
-        this->condition.notify_all();
-        for(int j=0;j<this->workers.size();j++){
-            workers[j].join();
-        }
-    }
-    
-    template<typename F,class... Args>
-auto enqueue(F&& f,Args&&... args) -> future<invoke_result_t<F,Args...>>{
-    using return_type=invoke_result_t<F,Args...>;
-     // 绑定函数和参数，创建packaged_task
-    auto task=make_shared<packaged_task<return_type()>>(bind(forward<F>(f),forward<Args>(args)...));
-    future<return_type> res=task->get_future();
+public:
+    explicit threadpool(size_t num);
+    ~threadpool();
+
+    template<typename F, class... Args>
+    auto enqueue(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>>;
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    std::condition_variable condition;
+    std::mutex mtx;
+    bool stop;
+};
+
+// 模板函数必须放在头文件
+template<typename F, class... Args>
+auto threadpool::enqueue(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>> {
+    using return_type = std::invoke_result_t<F, Args...>;
+
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+    std::future<return_type> res = task->get_future();
+
     {
-        unique_lock<mutex> lock(this->mtx);
-
-        if(this->stop) {
+        std::unique_lock<std::mutex> lock(this->mtx);
+        if (this->stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
-        }
 
-        // 将任务包装为void()类型
-        this->tasks.emplace([task]{(*task)();});
+        this->tasks.emplace([task]() { (*task)(); });
     }
     this->condition.notify_one();
     return res;
 }
-    
-    private:
-        vector<thread> workers; // 线程池中的线程
-        queue<function<void()>> tasks; // 任务队列
-        condition_variable condition; // 条件变量
-        mutex mtx; // 互斥量
-        bool stop; // 停止标志
-    };
 
-#endif
+#endif // THREADPOOL_HPP
