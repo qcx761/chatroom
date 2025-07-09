701965b836d2dec887ad6e3a360e65b305e7d10c
qcx761 <3216563979@qq.com>
2025-07-09 19:49:49 +0800

    1

--- chat-system/server/main.cpp
+++ chat-system/server/main.cpp
@@ -2,10 +2,10 @@
 
 int main(int argc, char* argv[]) {
     
-    // std::string port_str(argv[1]);
-    // int port = std::stoi(port_str);
+    std::string port_str(argv[1]);
+    int port = std::stoi(port_str);
 
-    // Server server(port);
+    Server server(port);
 
     return 0;
 }
